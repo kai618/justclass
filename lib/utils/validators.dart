c5334ac63e03c5ea7094def076244ca244cf493c
kai618 <vanhieu1601@gmail.com>
2020-06-24 17:43:48 +0700

    added trim() to every empty string validator

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -2,20 +2,20 @@ final emailRegExp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z
 
 class EmailPassValidator {
   static String validateEmail(String email) {
-    if (email.isEmpty) return "Please provide an email";
+    if (email.trim().isEmpty) return "Please provide an email";
     if (!emailRegExp.hasMatch(email)) return "Invalid email";
     return null;
   }
 
   static String validatePassword(String pass) {
-    if (pass.isEmpty) return "Please provide a password";
+    if (pass.trim().isEmpty) return "Please provide a password";
     if (pass.length < 6) return 'Password must have at least 6 characters!';
     // TODO: check password strength
     return null;
   }
 
   static String validateConfirm(String confirm, String pass) {
-    if (confirm.isEmpty) return "Please provide a confirm password";
+    if (confirm.trim().isEmpty) return "Please provide a confirm password";
     if (confirm != pass) return "Confirm password does not match";
     return null;
   }
@@ -23,14 +23,14 @@ class EmailPassValidator {
 
 class CreateClassValidator {
   static String validateClassTitle(String name) {
-    if (name.isEmpty) return "Please provide a class name";
+    if (name.trim().isEmpty) return "Please provide a class name";
     return null;
   }
 }
 
 class NewNoteValidator {
   static String validateNote(String note) {
-    if (note.isEmpty) return "Please provide a note";
+    if (note.trim().isEmpty) return "Please provide a note";
     const len = 1;
     if (note.length < len) return "A note needs to contain at least $len ${len == 1 ? "character" : "characters"}";
     // TODO: check string contains only break lines
@@ -40,7 +40,7 @@ class NewNoteValidator {
 
 class JoinClassValidator {
   static String validateCode(String code) {
-    if (code.isEmpty) return "Please provide a note";
+    if (code.trim().isEmpty) return "Please provide a code";
     const len = 8;
     if (code.length != len) return "Class code must contain $len ${len == 1 ? "character" : "characters"}";
     return null;
